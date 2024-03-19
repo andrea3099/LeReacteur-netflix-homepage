d3b06b03d347c6f7c6759a5b50803082378298d0
andrea3099 <143407725+andrea3099@users.noreply.github.com>
2024-03-19 16:57:11 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import data from "./assets/movies_rnexgr.json";
+import logoNetflix from "./assets/img/logo-netflix.png";
+
+function App() {
+  // console.log(data);
+  return (
+    <>
+      <div className="container">
+        <div className="container-logo">
+          <img src={logoNetflix} alt="logo-netflix" />
+        </div>
+        {data.map((moovies) => {
+          return (
+            <div className="container-category-image">
+              <div className="container-category">{moovies.category}</div>
+              <div className="container-img">
+                {moovies.images.map((affiches) => {
+                  return (
+                    <div className="img">
+                      <img src={affiches} alt="affiche-film" />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
