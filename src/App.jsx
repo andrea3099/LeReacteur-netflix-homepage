3cb50c401ad7c0c2b4078b189204d59b398d2664
andrea3099 <143407725+andrea3099@users.noreply.github.com>
2024-03-20 09:24:22 +0100

    ajout de la logique dans section

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
 import "./App.css";
 import data from "./assets/movies_rnexgr.json";
-import logoNetflix from "./assets/img/logo-netflix.png";
+import Section from "./components/Section";
 
 function App() {
   return (
     <>
-      <div className="container">
-        <div className="container-logo">
-          <img src={logoNetflix} alt="logo-netflix" />
-        </div>
-        {data.map((moovies) => {
-          return (
-            <div className="container-category-image">
-              <div className="container-category">{moovies.category}</div>
-              <div className="container-img">
-                {moovies.images.map((affiches) => {
-                  return (
-                    <div className="img">
-                      <img src={affiches} alt="affiche-film" />
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      <Section data={data} />
     </>
   );
 }
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,12 +1,27 @@
+import logoNetflix from "../assets/img/logo-netflix.png";
+
 const Section = (props) => {
   return (
-    <div className="container-category-image">
-      <div className="container-category">{props.title}</div>
-      <div className="container-img">
-        <div className="img">
-          <img src={props.list} alt="affiche-film" />
-        </div>
+    <div className="container">
+      <div className="container-logo">
+        <img src={logoNetflix} alt="logo-netflix" />
       </div>
+      {props.data.map((moovies) => {
+        return (
+          <div className="container-category-image">
+            <div className="container-category">{moovies.category}</div>
+            <div className="container-img">
+              {moovies.images.map((affiches) => {
+                return (
+                  <div className="img">
+                    <img src={affiches} alt="affiche-film" />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
