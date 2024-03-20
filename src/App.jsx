d985f69cbedc911de9d1663549bd94030a5f1ead
andrea3099 <143407725+andrea3099@users.noreply.github.com>
2024-03-20 12:35:50 +0100

    correction section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import data from "./assets/movies_rnexgr.json";
 import Section from "./components/Section";
+import logoNetflix from "./assets/img/logo-netflix.png";
 
 function App() {
   return (
     <>
-      <Section data={data} />
+      <div className="container">
+        <div className="container-logo">
+          <img src={logoNetflix} alt="logo-netflix" />
+        </div>
+        {data.map((moovies) => {
+          return <Section moovies={moovies} key={moovies.category} />;
+        })}
+      </div>
     </>
   );
 }
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,27 +1,16 @@
-import logoNetflix from "../assets/img/logo-netflix.png";
-
 const Section = (props) => {
   return (
-    <div className="container">
-      <div className="container-logo">
-        <img src={logoNetflix} alt="logo-netflix" />
-      </div>
-      {props.data.map((moovies) => {
-        return (
-          <div className="container-category-image">
-            <div className="container-category">{moovies.category}</div>
-            <div className="container-img">
-              {moovies.images.map((affiches) => {
-                return (
-                  <div className="img">
-                    <img src={affiches} alt="affiche-film" />
-                  </div>
-                );
-              })}
+    <div className="container-category-image">
+      <div className="container-category">{props.moovies.category}</div>
+      <div className="container-img">
+        {props.moovies.images.map((affiches) => {
+          return (
+            <div key={affiches} className="img">
+              <img src={affiches} alt="affiche-film" />
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
